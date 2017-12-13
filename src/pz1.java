0fd5f42e992d14bd694452937782a0a89ec5b215
WheatTail <hartovsky@gmail.com>
2017-12-13 09:40:18 +0300

    Add missing 1st lab

---
+++ src/pz1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class pz1 {
+    static Scanner reader = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Введите два числа");
+        int a = reader.nextInt();
+        int b = reader.nextInt();
+        int c=a+b;
+        System.out.println("+: "+ c);
+        c=a-b;
+        System.out.println("-: "+ c);
+        c=a*b;
+        System.out.println("*: "+c);
+        c=a/b;
+        System.out.println("/: "+c);
+    }
+}
